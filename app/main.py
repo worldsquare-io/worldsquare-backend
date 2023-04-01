4954d940cb2e9227e94acfe36df9c753c64aea49
lukvmil <millerluke1364@gmail.com>
2023-04-01 20:16:27 +0000

    added cors

--- app/main.py
+++ app/main.py
@@ -2,6 +2,7 @@ import datetime
 import json
 import nanoid
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from pydantic import BaseModel
 from mongoengine import connect
 from app.model import ItemModel, CreateRoot, CreateReply
@@ -9,11 +10,23 @@ from app.model import ItemModel, CreateRoot, CreateReply
 connect('worldsquare')
 app = FastAPI()
 
+origins = [
+    "https://worldsquare.io"
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"]
+)
+
 @app.get("/")
 def index():
     return {"message": "You've found the worldsquare API endpoint! Maybe you are looking for https://worldsquare.io?"}
 
-@app.post("/items/")
+@app.post("/items")
 def post_item(root: CreateRoot):
     item = ItemModel(
         oid = nanoid.generate(),
@@ -23,17 +36,19 @@ def post_item(root: CreateRoot):
     item.save()
     return json.loads(item.to_json())
 
-@app.get("/items/")
+@app.get("/items")
 def get_items():
     items = ItemModel.objects()
     return [json.loads(i.to_json()) for i in items]
 
 @app.get("/items/{oid}")
-def get_item():
+def get_item(oid):
     item = ItemModel.objects(pk=oid).first()
+    return json.loads(item.to_json())
 
 @app.post("/items/{oid}/replies")
 def post_replies(oid, reply: CreateReply):
+    print(reply.dict())
     item = ItemModel(
         oid = nanoid.generate(),
         variant = "local",
@@ -46,4 +61,5 @@ def post_replies(oid, reply: CreateReply):
 
 @app.get("/items/{oid}/replies")
 def get_replies(oid):
-    ItemModel.objects(parent=oid)
+    items = ItemModel.objects(parent=oid)
+    return [json.loads(i.to_json()) for i in items]
